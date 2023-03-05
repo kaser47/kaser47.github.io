e14814af58bedfc02e2ccb623c460e59477df976
DESKTOP-S161CAG\kaser <kaser47@hotmail.com>
2023-03-05 17:26:29 +0000

    Added new datasource

--- RecentlyAddedShows.Web/RecentlyAddedShows.Service/Configuration.cs
+++ RecentlyAddedShows.Web/RecentlyAddedShows.Service/Configuration.cs
@@ -2,9 +2,6 @@
 {
     public static class Configuration
     {
-        public const string ConnectionString =
-            "workstation id=rasDatabase.mssql.somee.com;packet size=4096;user id=kaser47_SQLLogin_1;" 
-            +"pwd=123qweasd;data source=rasDatabase.mssql.somee.com;persist security info=False;"
-            +"initial catalog=rasDatabase";
+        public const string ConnectionString = @"Server=sql.bsite.net\MSSQL2016;Database=recentlyaddedshows_ras;User Id=recentlyaddedshows_ras; Password=123qweasd;";
     }
 }
